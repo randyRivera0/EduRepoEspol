8dceff8d0c4ad6d29386a5725094615569afda2c
randyRivera0 <rariberm@espol.edu.ec>
2024-08-31 06:53:40 -0500

    modified:   pom.xml 	modified:   src/main/java/ec/edu/espol/edurepoespol/App.java 	modified:   src/main/java/ec/edu/espol/edurepoespol/DataBaseConnection.java 	new file:   src/main/java/ec/edu/espol/edurepoespol/PDFViewer.java 	deleted:    target/classes/DataBaseConnection.class 	modified:   target/classes/ec/edu/espol/edurepoespol/App.class 	new file:   target/classes/ec/edu/espol/edurepoespol/DataBaseConnection.class 	new file:   target/classes/ec/edu/espol/edurepoespol/PDFViewer.class 	new file:   target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst 	new file:   target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

--- src/main/java/ec/edu/espol/edurepoespol/App.java
+++ src/main/java/ec/edu/espol/edurepoespol/App.java
@@ -1,5 +1,6 @@
 package ec.edu.espol.edurepoespol;
 
+import java.io.File;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
--- src/main/java/ec/edu/espol/edurepoespol/DataBaseConnection.java
+++ src/main/java/ec/edu/espol/edurepoespol/DataBaseConnection.java
@@ -1,5 +1,6 @@
+package ec.edu.espol.edurepoespol;
 
-import java.util.Scanner;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
